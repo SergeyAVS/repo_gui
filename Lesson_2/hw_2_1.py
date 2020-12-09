98ea129a9019ad7d600c247545a67822f549a972
SergeyAV <cabcab07@mail.ru>
2020-12-09 15:04:36 +0400

    Initial commit

--- Lesson_2/hw_2_1.py
+++ Lesson_2/hw_2_1.py
@@ -1,10 +1,3 @@
 hw_21_list = [26, 8,'Zadacha', 56.7, 'Задача', None, True]
-def hw_21_type(el):
-    for el in  range(len(hw_21_list)):
-        print(type(hw_21_list[el]))
-    return
-hw_21_type(hw_21_list)
-
-
-print(hw_21_list)
-
+for el in hw_21_list:
+    print(type(el))
