315072164c8ab72f258429e6deea99dc9a913e87
SergeyAV <cabcab07@mail.ru>
2020-12-19 16:48:20 +0400

    Initial commit

--- Lesson_5/hw_5_1.py
+++ Lesson_5/hw_5_1.py
@@ -1,16 +1,21 @@
 # fname = input('Имя файла: ')
 f_obj = open('test_txt.txt', 'w')
 
-line = input('Введите текст:\n')
-while line:
-    f_obj.writelines(line)
-    line = input('Введите текст:\n')
-    if not line:
-        break
-
-f_obj.close()
-f_obj = open('test_txt.txt', 'r')
-content = f_obj.readlines()
-print(content)
+# line = input('Введите текст:\n')
+# while line:
+#     f_obj.writelines(line)
+#     line = input('Введите текст:\n')
+#     if not line:
+#         break
+#
+# f_obj.close()
+# f_obj = open('test_txt.txt', 'r')
+# content = f_obj.readlines()
+# print(content)
+# f_obj.close()
+while True:
+    s = input('Введите текс: ')
+    if s == '': break
+    f_obj.write(s+'\n')
 f_obj.close()
 
